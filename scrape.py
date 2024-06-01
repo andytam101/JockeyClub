defdd2aeaf4f063209ac6caaf6a0bab4f15174fd
andytam101 <andytam321@gmail.com>
2024-06-01 23:51:49 +0100

    added options to webdriver

--- scrape.py
+++ scrape.py
@@ -3,11 +3,12 @@ from selenium import webdriver
 
 def scrape(url: str) -> webdriver:
     options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
     options.add_argument("--headless")
-
+    options.add_argument("--disable-gpu")
+    
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(2)
 
     driver.get(url)
     return driver
-
