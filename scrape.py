e8e2e73492000f9f512feadf3139961cf815655c
andytam101 <andytam321@gmail.com>
2024-06-29 17:52:08 +0800

    added no console output

--- scrape.py
+++ scrape.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from subprocess import CREATE_NO_WINDOW
 
 
 def scrape(url: str) -> webdriver:
@@ -6,8 +8,12 @@ def scrape(url: str) -> webdriver:
     options.add_argument("--no-sandbox")
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    
-    driver = webdriver.Chrome(options=options)
+    options.add_argument("start-maximized")
+
+    service = ChromeService()
+    service.creation_flags = CREATE_NO_WINDOW
+
+    driver = webdriver.Chrome(options=options, service=service)
     driver.implicitly_wait(2)
 
     driver.get(url)
